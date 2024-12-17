fdd1cd61995597820829a20725178e2f1b340b8b
KauanRibeiroAzevedo <145453537+KauanRibeiroAzevedo@users.noreply.github.com>
2024-12-17 23:00:33 +0000

    start

--- src/ie/atu/testpackage/Main.java
+++ src/ie/atu/testpackage/Main.java
@@ -1,9 +1,11 @@
-package ie.atu.testpackage;
+package ie.atu.Drinksmanager;
 
- // This is the Main class. It contains the main method.
+
 public class Main {
-	// This is the main method.
+	
 	public static void main(String[] args) {
-		System.out.println("This is the main method. I am inside the Main calss.");
-	} // End main method
-} // End Main Class
+
+		Drinks drinksooObject = new drink(1, "Coca-Cola", 300000, false);
+		System.out.println(drinksoObject1.getBrand());
+	} 
+} 
