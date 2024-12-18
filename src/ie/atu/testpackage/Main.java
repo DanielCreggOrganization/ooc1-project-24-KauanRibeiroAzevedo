1bf743dd827e19eb286564e18ee0d299086bfb5a
KauanRibeiroAzevedo <145453537+KauanRibeiroAzevedo@users.noreply.github.com>
2024-12-18 01:38:18 +0000

    main done but with errors

--- src/ie/atu/testpackage/Main.java
+++ src/ie/atu/testpackage/Main.java
@@ -1,11 +1,27 @@
-package ie.atu.Drinksmanager;
-
+package ie.atu.testpackage;
 
 public class Main {
-	
-	public static void main(String[] args) {
+public static void main(String[] args) {
 
-		Drinks drinksooObject = new drink(1, "Coca-Cola", 300000, false);
-		System.out.println(drinksoObject1.getBrand());
-	} 
-} 
+			Scanner userInput = new Scanner(System.in);
+			int user Selection = 0;
+
+			while (true) {
+					System.out.println("");
+					System.out.println("(1) Add a Drink. ");
+					System.out.println("(2) Delete a drink. ");
+					System.out.println("(3) Show a total number of drinks.");
+					System.out.println("(4) Search for a Drink by ID. ");
+					System.out.println("(5) Quit");
+					System.out.println("Select an option from 1 to 5 and press Enter>");
+
+					user selection = userInput.nextInt();
+
+					if (userSelection == 5) {
+						System.out.println("Drink Application closing - Goodbye!");
+						userInput.close();
+						break
+			}
+		}
+	}
+}	
